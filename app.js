4563ed5dd4267b82863692d66781d0d9f947eedd
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2017-03-05 20:22:45 -0500

    maria lo jodio

--- app.js
+++ app.js
@@ -57,44 +57,53 @@ apiRoutes.get('/', function(req,res){
 //All of our routes will be prefixed with /api
 app.use('/api', apiRoutes);
 
-app.get('/account', (req, res) => res.render('account.html'));
+app.use('/api/s3', require('react-dropzone-s3-uploader/s3router')({
+    bucket: 'MyS3Bucket',
+    region: 'us-east-1', //optional 
+    headers: {'Access-Control-Allow-Origin': '*'}, // optional 
+    ACL: 'private' // this is default 
+}));
 
-app.get('/sign-s3', (req, res) => {
-	console.log('sign-s3');
-  const s3 = new aws.S3();
-  const fileName = req.query['file-name'];
-  const fileType = req.query['file-type'];
-  const s3Params = {
-    Bucket: S3_BUCKET,
-    Key: fileName,
-    Expires: 60,
-    ContentType: fileType,
-    ACL: 'public-read'
-  };
-  console.log('sign-s3');
+// app.get('/account', (req, res) => res.render('account.html'));
 
-  s3.getSignedUrl('putObject', s3Params, (err, data) => {
-    if(err){
-      console.log(err);
-      return res.end();
-    }
-    const returnData = {
-      signedRequest: data,
-      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
-    };
-    console.log('data');
-    console.log(returnData, 'data');
-    console.log('after data');
-    res.write(JSON.stringify(returnData));
-    res.end();
-  });
-});
+// //AMAZON S3
+// app.get('/sign-s3', (req, res) => {
+// 	console.log('sign-s3');
+//   const s3 = new aws.S3();
+//   const fileName = req.query['file-name'];
+//   const fileType = req.query['file-type'];
+//   const s3Params = {
+//     Bucket: S3_BUCKET,
+//     Key: fileName,
+//     Expires: 60,
+//     ContentType: fileType,
+//     ACL: 'public-read'
+//   };
+//   console.log('sign-s3');
 
-app.post('/save-details', (req, res) => {
-	// console.log(req.data);
-  // TODO: Read POSTed form data and do something useful
-});
+//   s3.getSignedUrl('putObject', s3Params, (err, data) => {
+//     if(err){
+//       console.log(err);
+//       return res.end();
+//     }
+//     const returnData = {
+//       signedRequest: data,
+//       url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+//     };
+//     console.log('data');
+//     console.log(returnData, 'data');
+//     console.log('after data');
+//     res.write(JSON.stringify(returnData));
+//     res.end();
+//   });
+// });
 
+// app.post('/save-details', (req, res) => {
+// 	// console.log(req.data);
+//   // TODO: Read POSTed form data and do something useful
+// });
+
+//AMAZON S3
 // START THE SERVER
 // ==============================================
 
