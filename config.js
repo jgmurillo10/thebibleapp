91a3842a2347fb81d93636979953a32f34657638
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-03-06 21:59:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 'use strict';
+
+//Its better to extract this URL to an Environment Variable, right now I can access your DB and delete all entries.
 module.exports = {
   'port': process.env.PORT || 3000,
   'url' : 'mongodb://admin:thebible@ds161059.mlab.com:61059/thebible'
